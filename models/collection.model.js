d567a4d46859a3c41d3ffaaa278ce52326ae8f65
devingprice <devingprice@gmail.com>
2019-06-11 20:45:20 -0400

    Add order to usercollections & update packages

--- models/collection.model.js
+++ models/collection.model.js
@@ -1,21 +1,25 @@
-const {TE, to}              = require('../services/util.service');
-
 module.exports = (sequelize, DataTypes) => {
-	var Model = sequelize.define('Collection', {
-		name: DataTypes.STRING
-	}, {
-		timestamps:false
-	});
+  var Model = sequelize.define(
+    'Collection',
+    {
+      name: DataTypes.STRING
+    },
+    {
+      timestamps: false
+    }
+  );
 
-	Model.associate = function(models){
-		this.Users = this.belongsToMany(models.User, {through: 'UserCollection'});
-		this.Channels = this.hasMany(models.Channel);
-	};
+  Model.associate = function(models) {
+    this.Users = this.belongsToMany(models.User, {
+      through: 'UserCollection'
+    });
+    this.Channels = this.hasMany(models.Channel);
+  };
 
-	Model.prototype.toWeb = function (pw) {
-		let json = this.toJSON();
-		return json;
-	};
+  Model.prototype.toWeb = function(pw) {
+    let json = this.toJSON();
+    return json;
+  };
 
-	return Model;
+  return Model;
 };
