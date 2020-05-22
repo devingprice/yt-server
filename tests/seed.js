bbbe656b95645ce54fb5463e27a8a59efe6f7637
Devin Price <devingprice@gmail.com>
2020-05-22 00:50:30 -0400

    Add db seed for tests

---
+++ tests/seed.js
@@ -0,0 +1,31 @@
+module.exports = function seed(models) {
+  let dumbyUser = {
+    first: 'Dumb',
+    last: 'Dumby',
+    email: 'dumbyTest@email.com',
+    password: 's3cureP@ss',
+    phone: '1234567890'
+  };
+
+  let collection = {
+    name: 'Pew and Top'
+  };
+
+  let channels = [
+    { name: 'PewDiePie', ytId: 'UC-lHJZR3Gqxm24_Vd_AJ5Yw' },
+    { name: 'TopMovieClips', ytId: 'UClVbhSLxwws-KSsPKz135bw' }
+  ];
+
+  return models.Collection.create(
+    {
+      ...collection,
+      owner: dumbyUser,
+      Channels: channels
+    },
+    {
+      include: [{ model: models.User, as: 'owner' }, models.Channel]
+    }
+  )
+    .then(() => models.UserCollection.create({ CollectionId: 1, UserId: 1 })) // may remove if db issue figured out
+    .catch(e => console.log(e));
+};
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,7 +1,8 @@
 process.env.NODE_ENV = 'test';
 process.env.APP = 'test';
 
-//let Book = require('../app/models/book');
+const models = require('../models');
+const seed = require('./seed');
 
 //Require the dev-dependencies
 let chai = require('chai');
@@ -11,20 +12,46 @@ let server = require('../app.js');
 let should = chai.should();
 
 chai.use(chaiHttp);
-describe('Testing Test', () => {
-  // beforeEach(done => {
-  //   Book.remove({}, err => {
-  //     done();
-  //   });
-  // });
+describe('Testing Users', () => {
+  beforeEach(done => {
+    models.sequelize
+      .sync({ force: true, match: /_test$/, logging: false })
+      .then(() => {
+        return seed(models);
+      })
+      .then(() => done());
+  });
 
-  describe('/GET index route', () => {
-    it('it should return a success response', done => {
+  describe('POST /users', () => {
+    it('it should not POST a user without email included', done => {
       chai
         .request(server)
-        .get('/')
+        .post('/users')
         .end((err, res) => {
+          res.body.success.should.equal(false);
           res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('POST /users', () => {
+    it('it should POST a user with correct details', done => {
+      let userDetails = {
+        email: 'test@gmail.com',
+        password: 's3cureP@ss'
+      };
+      chai
+        .request(server)
+        .post('/users')
+        .send(userDetails)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.should.have.property('user');
+          res.body.user.should.have.property('id');
+          res.body.should.have.property('token');
+          res.body.success.should.equal(true);
           done();
         });
     });
