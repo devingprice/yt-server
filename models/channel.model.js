ce4c7ed79dccb50de7e1cfcd8c26635e612df44c
Devin Price <devingprice@gmail.com>
2020-05-24 23:49:12 -0400

    Remove old comment from channel model

--- models/channel.model.js
+++ models/channel.model.js
@@ -10,11 +10,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
 
-  /* In sequelize docs:
-	 Country.hasMany(City, {foreignKey: 'countryCode', sourceKey: 'isoCode'});
-	 City.belongsTo(Country, {foreignKey: 'countryCode', targetKey: 'isoCode'});
-	but im not sure I need this if I just have has many on parent, testing now
-	 */
   Model.associate = function(models) {
     this.Collections = this.belongsTo(models.Collection);
   };
