61f5987d87be02599e700a582b0b82828702f179
Devin Price <devingprice@gmail.com>
2020-05-24 23:12:32 -0400

    Remove references to home controller

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const UserController = require('../controllers/user.controller');
-//const HomeController = require('../controllers/home.controller');
 const CollectionController = require('../controllers/collection.controller');
 const ChannelController = require('../controllers/channel.controller');
 
 const custom = require('./../middleware/custom');
-
 const passport = require('passport');
 const path = require('path');
 
 require('./../middleware/passport')(passport);
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.status(200).json({
     status: 'success',
@@ -26,28 +23,20 @@ router.get('/', function(req, res) {
 
 const JWTAuth = passport.authenticate('jwt', { session: false });
 
-router.post('/users', UserController.create); // C
-router.get('/users', JWTAuth, UserController.get); // R
-router.put('/users', JWTAuth, UserController.update); // U
-router.delete('/users', JWTAuth, UserController.remove); // D
+router.post('/users', UserController.create);
+router.get('/users', JWTAuth, UserController.get);
+router.put('/users', JWTAuth, UserController.update);
+router.delete('/users', JWTAuth, UserController.remove);
 router.post('/users/login', UserController.login);
 
-/*
-router.get(
-  '/dash',
-  passport.authenticate('jwt', { session: false }),
-  HomeController.Dashboard
-);
-*/
-
 router.post('/collection', JWTAuth, CollectionController.create);
-router.get('/collections/:user_id', JWTAuth, CollectionController.getAllForUser); // R
-router.get('/collection/:collection_id', JWTAuth, custom.collection, CollectionController.get); // R
-router.put('/collection/:collection_id', JWTAuth, custom.collection, CollectionController.update); // U
-router.delete('/collection/:collection_id', JWTAuth, custom.collection, CollectionController.remove); // D
+router.get('/collections/:user_id', JWTAuth, CollectionController.getAllForUser);
+router.get('/collection/:collection_id', JWTAuth, custom.collection, CollectionController.get);
+router.put('/collection/:collection_id', JWTAuth, custom.collection, CollectionController.update);
+router.delete('/collection/:collection_id', JWTAuth, custom.collection, CollectionController.remove);
 
-router.post('/channel/:collection_id', JWTAuth, custom.collection, ChannelController.create); // U
-router.delete('/channel/:channel_id', JWTAuth, custom.channel, ChannelController.remove); // D
+router.post('/channel/:collection_id', JWTAuth, custom.collection, ChannelController.create);
+router.delete('/channel/:channel_id', JWTAuth, custom.channel, ChannelController.remove);
 
 router.post('/collections', JWTAuth, CollectionController.create);
 
