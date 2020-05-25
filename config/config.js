6e857c59c568bf31f7e42a103ba955ad6806eef0
Devin Price <devingprice@gmail.com>
2020-05-24 23:13:34 -0400

    Remove old comments from config

--- config/config.js
+++ config/config.js
@@ -1,24 +1,4 @@
-// require('dotenv').config(); //instatiate environment variables
-
-// let CONFIG = {}; //Make this global to use all over the application
-
-// console.log("NODE_ENV is " + process.env.NODE_ENV);
-
-// CONFIG.app = process.env.APP || 'dev';
-// CONFIG.port = process.env.PORT || '3001';
-
-// CONFIG.db_dialect = process.env.DB_DIALECT || 'mysql';
-// CONFIG.db_host = process.env.DB_HOST || 'localhost';
-// CONFIG.db_port = process.env.DB_PORT || '3306';
-// CONFIG.db_name = process.env.DB_NAME || 'ytServer';
-// CONFIG.db_user = process.env.DB_USER || 'root';
-// CONFIG.db_password = process.env.DB_PASSWORD || 'P@ssw0rd';
-
-// CONFIG.jwt_encryption = process.env.JWT_ENCRYPTION || 'jwt_please_change';
-// CONFIG.jwt_expiration = process.env.JWT_EXPIRATION || '10000';
-
-// module.exports = CONFIG;
-
+/* eslint-disable camelcase */
 const { resolve } = require('path');
 
 switch (process.env.NODE_ENV) {
